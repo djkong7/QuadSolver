c797b23ca0626193f4a9bc4be66695391ef4897b
Danny Kiel <daniel.j.kiel@wmich.edu>
2018-02-12 12:09:04 -0500

    Updated with todays class stuff.

--- src/validate/validate.c
+++ src/validate/validate.c
@@ -1,4 +1,4 @@
 //double a,b,c;
-//ret = validate(line, n, &a, &b, &c):
+//ret = validate(char *line, int n, double &a, double &b, double &c);
 //check a!=0
 //check all are numeric and doubles
---
+++ src/validate/validate.h
@@ -0,0 +1,1 @@
+int validate(char *line, int n, double &a, double &b, double &c);
