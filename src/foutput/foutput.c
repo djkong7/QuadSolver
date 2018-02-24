467e2cda5e179b8878a5e404f04beab9365e7750
mpeter97 <mhpeter97@gmail.com>
2018-02-24 14:07:42 -0500

    Added some exit()s instead of breaks
    Exit.....now

--- src/foutput/foutput.c
+++ src/foutput/foutput.c
@@ -16,30 +16,31 @@ void foutput(int errorNumber){
 			if (wline("rline() Failed.\n") != 0) {
 			    err_sys("Error printing failed!");
 			}
-			break;
+			exit(1);
 		case -2:
 			if (wline("wline() Failed.\n") != 0) {
 			    err_sys("Error printing failed!");
 			}
-			break;
+			exit(1);
 		case -3:
 			if (wline("3 input values required.\n") != 0) {
 			    err_sys("Error printing failed!");
 			}
-			break;
+			exit(1);
 		case -4:
 			if (wline("Invalid characters entered.\n") != 0) {
 			    err_sys("Error printing failed!");
 			}
-			break;
+			exit(1);
 		case -5:
 			if (wline("First value cannot be 0.\n") != 0) {
 			    err_sys("Error printing failed!");
 			}
-			break;
+			exit(1);
 		case -6:
 		  	if (wline("Two complex roots. No result.\n") != 0) {
 			    err_sys("Error printing failed!");
 			}
+			exit(1);
 	}
 }
