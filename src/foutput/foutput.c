5b70039d3a55c8142d86953aa6226e416a69072f
mpeter97 <mhpeter97@gmail.com>
2018-02-24 13:58:40 -0500

    Added error prints and a success print
    SUCCESS!!

--- src/foutput/foutput.c
+++ src/foutput/foutput.c
@@ -7,33 +7,39 @@
 
 void foutput(int errorNumber){
 	switch (errorNumber) {
+		case 0:
+			if (wline("Success.\n") != 0) {
+			    err_sys("Error printing failed!");
+			}
+			break;
 		case -1:
 			if (wline("rline() Failed.\n") != 0) {
-			    // What do we do in this case?
+			    err_sys("Error printing failed!");
+			}
+			break;
+		case -2:
+			if (wline("wline() Failed.\n") != 0) {
+			    err_sys("Error printing failed!");
 			}
 			break;
 		case -3:
 			if (wline("3 input values required.\n") != 0) {
-			    // What do we do in this case?
+			    err_sys("Error printing failed!");
 			}
 			break;
 		case -4:
 			if (wline("Invalid characters entered.\n") != 0) {
-			    // What do we do in this case?
+			    err_sys("Error printing failed!");
 			}
 			break;
 		case -5:
 			if (wline("First value cannot be 0.\n") != 0) {
-
-			}
-		case -6:
-		  if (wline("Two complex roots. No result.\n") != 0) {
-				
-			}
-		default:
-			if (wline("Invalid error number.") != 0) {
-			    // What do we do in this case?
+			    err_sys("Error printing failed!");
 			}
 			break;
+		case -6:
+		  	if (wline("Two complex roots. No result.\n") != 0) {
+			    err_sys("Error printing failed!");
+			}
 	}
 }
