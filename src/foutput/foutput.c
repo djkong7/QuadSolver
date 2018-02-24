603ce04277c2ae6f9437937e1243d8ccf7169bc8
mpeter97 <mhpeter97@gmail.com>
2018-02-24 13:54:38 -0500

    Added some more error checks
    I like errors

--- src/foutput/foutput.c
+++ src/foutput/foutput.c
@@ -8,20 +8,28 @@
 void foutput(int errorNumber){
 	switch (errorNumber) {
 		case -1:
-			if (wline("rline() Failed.") != 0) {
+			if (wline("rline() Failed.\n") != 0) {
 			    // What do we do in this case?
 			}
 			break;
 		case -3:
-			if (wline("3 input values required.") != 0) {
+			if (wline("3 input values required.\n") != 0) {
 			    // What do we do in this case?
 			}
 			break;
 		case -4:
-			if (wline("Invalid characters entered.") != 0) {
+			if (wline("Invalid characters entered.\n") != 0) {
 			    // What do we do in this case?
 			}
 			break;
+		case -5:
+			if (wline("First value cannot be 0.\n") != 0) {
+
+			}
+		case -6:
+		  if (wline("Two complex roots. No result.\n") != 0) {
+				
+			}
 		default:
 			if (wline("Invalid error number.") != 0) {
 			    // What do we do in this case?
