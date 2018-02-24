88747dddfbab60df515b945610f6238358ceb8d1
Jonah Kubath <jkubath1@gmail.com>
2018-02-24 14:14:03 -0500

    Validate Update
    Validate function was changed to check for a loss in accuracy when encoding input to float numbers.

--- src/validate/test.c
+++ src/validate/test.c
@@ -1,1 +1,37 @@
 // test validate
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+#include "validate.h"
+
+int main() {
+  printf("Test Run\n");
+  FILE *f = fopen("validateTest.txt", "r");
+  char* lineRead = calloc(3, sizeof(char));
+  size_t len = 0;
+  ssize_t read;
+  int validateError = 0;
+  int count = 0;
+
+  //Values for validate
+  int n = 0;
+  double a;
+  double b;
+  double c;
+
+  while ((read = getline(&lineRead, &len, f)) != -1) {
+    validateError = validate(lineRead, n, &a, &b, &c);
+
+    if (validateError == -3) {
+      printf("ERROR: Test %d, input %s failed!\n", count, lineRead);
+    }
+    else if(validateError == -7){
+      printf("Test lost accuracy\n");
+    }
+
+    count++;
+
+  }
+
+  return 0;
+}
--- src/validate/validate.c
+++ src/validate/validate.c
@@ -3,8 +3,9 @@
 #include <math.h>
 #include <stdlib.h>
 #include <string.h>
+#include "validate.h"
 
-int validate(char* line, int n, double* a, double* b, double* c){
+int validate(char *line, int n, double *a, double *b, double *c){
 	for (int i = 0; line[i] != '\0' && line[i] != '\n'; i++) {
 		char cur = line[i];
 		if ((cur < 48 && cur != 32 && cur != 46 && cur != 45) || (cur > 57 && cur != 69 && cur != 101)) {
@@ -31,11 +32,28 @@ int validate(char* line, int n, double* a, double* b, double* c){
 		i++;
 	}
 
+	//Check for lost accuracy
+	float aFloat = *a;
+	float bFloat = *b;
+	float cFloat = *c;
+
+	int accLost = 0;
+
+	//The floats are not equal to their double equivalents
+	if(*a != (double) aFloat || *b != (double) bFloat || *c != (double) cFloat){
+		accLost = 1;
+	}
+
 	//only 3 numbers inputted...
-	if(i==3){
+	if(i==3 && accLost == 0){
 		//all good
-		return 1;
-	} else {
+		return 0;
+	}
+	else if(i==3 && accLost == 1){
+		//Accuracy was lost
+		return -7;
+	}
+	else {
 		//too many/too few numbers
 		return -3;
 	}
