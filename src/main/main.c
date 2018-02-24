362654d8e9f261c645d80e6e2f63beb6d8b789c4
mpeter97 <mhpeter97@gmail.com>
2018-02-24 13:39:10 -0500

    Simplified with calling foutput on function returns
    Makin' it simpler

--- src/main/main.c
+++ src/main/main.c
@@ -13,26 +13,12 @@
 int main(int argc, char** argv){
 
 	char input[256];
+	double a, b, c, x1, x2;
 
-	if (wline("Welcome to Quadratic Formula Solver.\nPlease enter A B C:\n") ==  -2) {
-		foutput(-2);
-	} else {
-		int ret = rline(256, &input[0]);
+	foutput(wline("Welcome to Quadratic Formula Solver.\nPlease enter A B C:\n"));
+	foutput(rline(256, &input[0]));
+	foutput(validate(&input[0], 256, &a, &b, &c));
+	foutput(qsolve(a, b, c, &x1, &x2));
 
-		if (ret == 0) {
-			double a,b,c;
-			ret = validate(&input[0], 256, &a, &b, &c);
-			if(ret == 1){
-				double x1, x2;
-				ret = qsolve(a, b, c, &x1, &x2);
-			} else {
-				//validate failed
-				foutput(ret);
-			}
-		} else {
-			//rline failed
-			foutput(ret);
-		}
-	}
 	return 0;							
 }
